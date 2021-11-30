590c2c7de246580fbb2a7a14297248974fbf086e
Themba <thembelanimahlangu@outlook.com>
2021-11-30 22:41:13 +0100

    Update main.jsx

--- assets/js/main.jsx
+++ assets/js/main.jsx
@@ -3,5 +3,4 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import Chart from './components/chart.jsx'
 
-const domContainer = document.querySelector('#chart-container');
 ReactDOM.render(<Chart/>, document.getElementById('chart-container'));
